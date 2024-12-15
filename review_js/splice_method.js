60730452114cf1c3cc3cf1d7b3e4e822603bf745
Muhammad Rizqi Ghana <mrizqighana@outlook.co.id>
2024-12-15 10:43:16 +0700

    Review materi JS

---
+++ review_js/splice_method.js
@@ -0,0 +1,8 @@
+let fruits = ["pisang", "mangga", "jeruk"]
+console.log(fruits)
+// splice memiliki 3 parameter
+// 1. urutan ke berapa yg mau diubah
+// 2.  mau diubah sampai berapa
+// 3. mau dirubah dengan apa
+fruits.splice(1,1,"anggur")
+console.log(fruits)
---
+++ review_js/ternary.js
@@ -0,0 +1,4 @@
+let bool = true
+// setelah tanda tanya jika kondisinya true
+// setelah titik dua jika kondisinya false
+bool ? console.log("ok") : console.log("not")
